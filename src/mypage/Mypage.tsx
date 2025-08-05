128abc5862075422b1409337482c2cb487b7ac83
bin00125 <ebin00125@gmail.com>
2025-08-05 11:15:35 +0900

    design: 마이페이지 레이아웃 조정

--- src/mypage/Mypage.tsx
+++ src/mypage/Mypage.tsx
@@ -1,14 +1,44 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function MyPage() {
   return (
-    <div>
-      <nav>
-        <Link to="/mypage">마이페이지</Link>
-        <Link to="/mypage/calendar">캘린더</Link>
-        <Link to="/mypage/challenge">챌린지</Link>
+    <div className="flex flex-col md:flex-row w-full h-full max-w-[1400px] m-auto p-[100px_0]">
+      <nav className="flex md:flex-col justify-center md:justify-start gap-[30px] md:w-[20%] mt-[5px]">
+        <NavLink
+          to="/mypage"
+          end
+          className={({ isActive }) =>
+            `text-[20px] font-bold tracking-[-.03rem] w-fit ${
+              isActive ? "text-[#0180F5]" : "text-[#878787]"
+            }`
+          }
+        >
+          마이페이지
+        </NavLink>
+        <NavLink
+          to="/mypage/calendar"
+          end
+          className={({ isActive }) =>
+            `text-[20px] font-bold tracking-[-.03rem] w-fit ${
+              isActive ? "text-[#0180F5]" : "text-[#878787]"
+            }`
+          }
+        >
+          캘린더
+        </NavLink>
+        <NavLink
+          to="/mypage/challenge"
+          end
+          className={({ isActive }) =>
+            `text-[20px] font-bold tracking-[-.03rem] w-fit ${
+              isActive ? "text-[#0180F5]" : "text-[#878787]"
+            }`
+          }
+        >
+          챌린지
+        </NavLink>
       </nav>
-      <section>
+      <section className="w-[90%] m-[50px_auto] md:m-[0] md:w-[80%]">
         <Outlet />
       </section>
     </div>
--- src/mypage/components/Calendar.tsx
+++ src/mypage/components/Calendar.tsx
@@ -1,7 +1,7 @@
 export default function Calendar() {
   return (
     <>
-      <h2>캘린더</h2>
+      <h2 className="text-4xl text-[#242424] tracking-[-.05rem]">캘린더</h2>
       <div></div>
     </>
   );
