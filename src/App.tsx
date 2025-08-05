3216e731f15b6b32f1c0c1463fe1867b44556fa1
bin00125 <ebin00125@gmail.com>
2025-08-05 14:04:14 +0900

    feat: header메뉴 반응형 ui구성 및 로그인 상태에 따른 분기 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./header/Header";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/mypage" />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
