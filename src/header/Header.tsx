c2ced07249f3560802b35f01ee7db66c68a2ffcc
jamminP <parkjaemin08@gmail.com>
2025-08-06 17:25:56 +0900

    feat: 헤더에 사이트 로고 추가

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 import bellIcon from "../header/img/bell.png";
 import { useState } from "react";
+import Logo from "./img/Logo.png";
 
 export default function Header({ isLoggedIn }: { isLoggedIn: boolean }) {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="fixed flex justify-between items-center w-full p-[20px_30px] md:p-[30px_60px] bg-[#ffffff] z-[999]">
-      <Link to="/">
-        <h1 className="text-[#0180F5] text-2xl font-extrabold">LOGO</h1>
+      <Link to="/" className="flex items-center">
+        <img src={Logo} alt="logo" className="h-12 w-auto" />
+        <h1 className="text-[#1B3043] text-4xl font-extrabold ml-3 mb-2">
+          Evi
+        </h1>
       </Link>
 
       <button className="md:hidden" onClick={() => setIsOpen((prev) => !prev)}>
@@ -17,10 +21,14 @@ export default function Header({ isLoggedIn }: { isLoggedIn: boolean }) {
           } w-[25px] h-[2px] bg-black mb-[5px]`}
         ></div>
         <div
-          className={`${isOpen ? "hidden" : "block"} w-[25px] h-[2px] bg-black mb-[5px]`}
+          className={`${
+            isOpen ? "hidden" : "block"
+          } w-[25px] h-[2px] bg-black mb-[5px]`}
         ></div>
         <div
-          className={`${isOpen ? "rotate-135" : "rotate-0"} w-[25px] h-[2px] bg-black`}
+          className={`${
+            isOpen ? "rotate-135" : "rotate-0"
+          } w-[25px] h-[2px] bg-black`}
         ></div>
       </button>
 
