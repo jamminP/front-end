1e80dd89c111ffc413beafbc57e320c8832e47b5
jamminP <parkjaemin08@gmail.com>
2025-08-06 14:58:46 +0900

    style: font 크기 수정

--- src/landing/components/BenefitRending.tsx
+++ src/landing/components/BenefitRending.tsx
@@ -3,10 +3,11 @@ import BenefitRendingImg from "../img/ready_to_next_level.jpg";
 
 function BenefitRending() {
   return (
-    <div className="relative w-full max-w-screen-xl mx-auto px-40 flex justify-between">
-      <div>
-        <h2 className="text-5xl font-bold">Ready to</h2>
-        <h2 className="text-5xl font-bold mt-4 mb-15">next level?</h2>
+    <div className="relative w-full max-w-screen-xl mx-auto px-40 flex justify-between items-center mt-12">
+      <div className="pr-10">
+        <h2 className="text-4xl font-bold">저희와 함께</h2>
+        <h2 className="text-4xl font-bold mt-4">AI를 활용하여</h2>
+        <h2 className="text-4xl font-bold mt-4 mb-10">계획 수립!</h2>
         <div className="pl-1 text-base font-medium text-gray-500 ">
           <p className="flex items-center gap-3">
             <FaCheck className="text-[#006ACB]" /> 공부 계획의 도움이
