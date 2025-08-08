5d0147544d8b128fab055278c3791ce12ef373bb
jamminP <parkjaemin08@gmail.com>
2025-08-08 13:03:16 +0900

    error: Footer 파일명 대소문자 충돌 해결 (TS1261)

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Challenge from './mypage/components/Challenge';
 import Login from './login/Login';
 import MyCalendar from './mypage/components/Calendar';
 import LandingPage from '@landing/LandingPage';
-import Footer from './footer/footer';
+import Footer from './footer/Footer';
 
 function App() {
   return (
