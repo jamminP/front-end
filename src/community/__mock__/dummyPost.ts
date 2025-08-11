afb88ccb0f37af9b964e124ac01e3ce6c9e160bb
Jaeeun0723 <lllbbch2@naver.com>
2025-08-11 13:57:43 +0900

    fix: Postform 코드 오류 수정 및 Postcard,더미데이터 타입 재지정

--- src/community/__mock__/dummyPost.ts
+++ src/community/__mock__/dummyPost.ts
@@ -1,40 +1,43 @@
 // src/Community/__mock__/dummyPosts.ts
 
-import { Post } from '../api/community';
+import { Post } from '../api/types';
 
 export const dummyPosts: Post[] = [
   {
     postId: 1,
     title: 'React 자료 모음.zip 공유합니다',
     author: '쟂니',
+    authorId: 2,
     category: 'share',
     content: 'React 공식 문서, 강의, 유튜브 모음입니다.',
     createdAt: '2025-08-06',
-    views: 120,
-    likes: 15,
-    comments: 3,
+    viewCount: 120,
+    likeCount: 15,
+    commentCount: 3,
   },
   {
     postId: 2,
     title: '오늘 너무 덥다...',
     author: '무무',
+    authorId: 3,
     category: 'free',
     content: '밖에 나갔다가 익을 뻔',
     createdAt: '2025-08-06',
-    views: 88,
-    likes: 4,
-    comments: 0,
+    viewCount: 88,
+    likeCount: 4,
+    commentCount: 0,
   },
   {
     postId: 3,
     title: 'React 스터디 주말 모집합니다',
     author: '에뷔의에뷔',
+    authorId: 4,
     category: 'study',
     content: '기초부터 실전까지 함께해요!',
     createdAt: '2025-08-06',
-    views: 230,
-    likes: 22,
-    comments: 7,
+    viewCount: 230,
+    likeCount: 22,
+    commentCount: 7,
     recruitStart: '2025-08-07',
     recruitEnd: '2025-08-15',
     studyStart: '2025-08-17',
