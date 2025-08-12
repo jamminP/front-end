ecc79323f66b4c18198f680c14a9ff8706dc2e06
jamminP <parkjaemin08@gmail.com>
2025-08-12 14:56:17 +0900

    style: LandingPage 반응형 추가

--- src/landing/LandingPage.tsx
+++ src/landing/LandingPage.tsx
@@ -1,10 +1,12 @@
 import BenefitRending from './components/BenefitRending';
 import MainRending from './components/MainRending';
 import FAQRending from './components/FAQRending';
+import HeroRending from './components/heroRending';
 
 function LandingPage() {
   return (
-    <div className="pt-30">
+    <div className="pt-17 md:pt-24">
+      <HeroRending />
       <MainRending />
       <FAQRending />
       <BenefitRending />
