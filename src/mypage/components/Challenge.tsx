7f04436d8c6f20fc06d11dfe2c4fcfcc182e5255
bin00125 <ebin00125@gmail.com>
2025-08-22 15:50:06 +0900

    feat: 챌린지 없을경우 텍스트 표시

--- src/mypage/components/Challenge.tsx
+++ src/mypage/components/Challenge.tsx
@@ -63,35 +63,45 @@ export default function Challenge() {
 
   return (
     <>
-      <h2 className="text-3xl md:text-4xl text-[#242424] tracking-[-.05rem] mb-[30px]">챌린지</h2>
-      <ul>
-        {myChallenge.map((c) => {
-          const status = calculateStatus(c);
-          return (
-            <>
-              <li
-                key={c.id}
-                className="flex justify-between md:items-center flex-col md:flex-row w-[100%] text-[#252525] bg-[#ffffff] rounded-2xl mb-[5%] md:mb-[2%] p-[25px] border-[1px] border-[#e9e9e9] transform transition-transform duration-300 hover:translate-y-[-5px]"
-              >
-                <div className="w-full md:w-[80%]">
-                  <h4 className="text-[1.1rem] font-bold tracking-[-.03rem] leading-[1.3]">
-                    {c.input_data}
-                  </h4>
-                  <p className="text-[.9rem] text-[#797979] m-[10px_0] truncate">{c.output_data}</p>
-                  <span className="text-[.8rem] text-[#c2c2c2]">
-                    {c.start_date}~{c.end_date}
-                  </span>
-                </div>
-                <div
-                  className={`flex justify-center items-center text-[.9rem] mt-[15px] md:mt-[0] p-[3px_10px] md:p-[5px] w-fit md:w-[64px] md:h-[64px] rounded-4xl text-[#ffffff] ${status === '진행 중' ? 'bg-[#f1513c]' : status === '완료' ? 'bg-[#723932]' : 'bg-[#1b3043]'}`}
+      {myChallenge.length === 0 ? (
+        <p className="text-[1.2rem] text-[#999] font-light tracking-[-0.03rem] mt-5 pl-[5px]">
+          등록된 챌린지가 없습니다.
+        </p>
+      ) : (
+        <>
+          <h2 className="text-3xl md:text-4xl text-[#242424] tracking-[-.05rem] mb-[30px]">
+            챌린지
+          </h2>
+          <ul>
+            {myChallenge.map((c) => {
+              const status = calculateStatus(c);
+              return (
+                <li
+                  key={c.id}
+                  className="flex justify-between md:items-center flex-col md:flex-row w-[100%] text-[#252525] bg-[#ffffff] rounded-2xl mb-[5%] md:mb-[2%] p-[25px] border-[1px] border-[#e9e9e9] transform transition-transform duration-300 hover:translate-y-[-5px]"
                 >
-                  {status}
-                </div>
-              </li>
-            </>
-          );
-        })}
-      </ul>
+                  <div className="w-full md:w-[80%]">
+                    <h4 className="text-[1.1rem] font-bold tracking-[-.03rem] leading-[1.3]">
+                      {c.input_data}
+                    </h4>
+                    <p className="text-[.9rem] text-[#797979] m-[10px_0] truncate">
+                      {c.output_data}
+                    </p>
+                    <span className="text-[.8rem] text-[#c2c2c2]">
+                      {c.start_date}~{c.end_date}
+                    </span>
+                  </div>
+                  <div
+                    className={`flex justify-center items-center text-[.9rem] mt-[15px] md:mt-[0] p-[3px_10px] md:p-[5px] w-fit md:w-[64px] md:h-[64px] rounded-4xl text-[#ffffff] ${status === '진행 중' ? 'bg-[#f1513c]' : status === '완료' ? 'bg-[#723932]' : 'bg-[#1b3043]'}`}
+                  >
+                    {status}
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      )}
     </>
   );
 }
