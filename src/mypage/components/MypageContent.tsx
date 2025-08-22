7e25824ba93cd11185ad27fee0d7e982115b9100
bin00125 <ebin00125@gmail.com>
2025-08-22 11:11:10 +0900

    chore: 닉네임 컴포넌트 분리

--- src/mypage/components/MypageContent.tsx
+++ src/mypage/components/MypageContent.tsx
@@ -1,45 +1,10 @@
 import useAuthStore from '@src/store/authStore';
-import { useState } from 'react';
-import axios from 'axios';
 import MyPostsSection from './MyPostsSection';
 import LikedPostsSection from './LikedPostsSection';
+import NicknameSection from './NicknameSection';
 
 export default function MypageContent() {
-  const [nicknameInput, setNicknameInput] = useState('');
-  const [nicknameModal, setNicknameModal] = useState(false);
   const user = useAuthStore((state) => state.user);
-  const setAuthData = useAuthStore((state) => state.setAuthData);
-
-  // 닉네임 수정 모달
-  const openNicknameModal = () => {
-    setNicknameInput(user?.nickname || '');
-    setNicknameModal(true);
-  };
-  const closeNicknameModal = () => setNicknameModal(false);
-
-  const handleNicknameUpdate = async () => {
-    if (!nicknameInput.trim()) {
-      alert('닉네임을 입력해주세요');
-      return;
-    }
-    try {
-      const res = await axios.patch(
-        'https://backend.evida.site/api/v1/users/myinfo',
-        { nickname: nicknameInput },
-        { withCredentials: true },
-      );
-      if (user) {
-        setAuthData({
-          ...user,
-          nickname: res.data.new_nickname,
-        });
-        alert('수정되었습니다');
-      }
-      closeNicknameModal();
-    } catch (err) {
-      console.error('닉네임 변경 실패', err);
-    }
-  };
 
   return (
     <>
@@ -57,41 +22,7 @@ export default function MypageContent() {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="p-[6px_12px] md:p-[10px_15px] bg-[#1b3043] text-[#ffffff] text-[.9rem] rounded-[100px] font-semibold cursor-pointer"
-          onClick={openNicknameModal}
-        >
-          닉네임 수정
-        </button>
-
-        {nicknameModal && (
-          <div className="modal-overlay" onClick={closeNicknameModal}>
-            <div
-              className="relative w-full max-w-[380px] bg-[#ffffff] rounded-[20px] p-[30px]"
-              onClick={(e) => e.stopPropagation()}
-            >
-              <h2 className="text-[1.1rem] text-[#313131] font-bold mb-[10px]">닉네임 수정하기</h2>
-              <div className="md:flex items-center justify-between">
-                <input
-                  type="text"
-                  maxLength={10}
-                  value={nicknameInput}
-                  onChange={(e) => setNicknameInput(e.target.value)}
-                  className="w-full md:w-[82%] border-[1px] border-[#d1d1d1] rounded-[10px] p-[10px] mr-[10px] mb-[10px] md:mb-[0px]"
-                />
-                <div className="modal-button">
-                  <button onClick={handleNicknameUpdate}>수정</button>
-                </div>
-              </div>
-
-              <button className="modal-close" onClick={closeNicknameModal}>
-                <span></span>
-                <span></span>
-              </button>
-            </div>
-          </div>
-        )}
+        <NicknameSection />
       </div>
 
       <MyPostsSection />
