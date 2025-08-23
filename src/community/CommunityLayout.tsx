63a003e86a8ed735bc398df62a14adda3db12ce1
Jaeeun0723 <lllbbch2@naver.com>
2025-08-24 01:06:35 +0900

    refactor: 더미데이터 제거

--- src/community/CommunityLayout.tsx
+++ src/community/CommunityLayout.tsx
@@ -3,31 +3,6 @@ import CommunityTab from './components/CommunityTab';
 import SidebarRanking from './components/SidebarRanking';
 import CreatePostButton from './components/CreatePostButton';
 
-const dummyfreePost = [
-  { id: 1, title: '오늘 날씨가 좋아요' },
-  { id: 2, title: '2번 제목' },
-  { id: 3, title: '저녁 뭐 먹지?' },
-  { id: 4, title: '아무말' },
-  { id: 5, title: '에뷔 좋네요' },
-];
-
-const dummyStudyPost = [
-  { id: 11, title: '스터디 일정 공유' },
-  { id: 12, title: 'React 공부 팁' },
-  { id: 13, title: '스터디 인원 모집' },
-  { id: 14, title: '이번 주 목표' },
-  { id: 15, title: '공부 습관 공유' },
-];
-
-const dummySharePost = [
-  { id: 16, title: '자료 공유' },
-  { id: 17, title: '개념 공유' },
-  { id: 18, title: '이것저것 공유' },
-  { id: 19, title: '공유' },
-  { id: 20, title: '도깨비' },
-];
-// 더미데이터로 구현 확인
-
 const CommunityLayout = () => {
   const location = useLocation();
 
