90038b1ab2005f6792437d6c5eb09127b29f9681
bin00125 <ebin00125@gmail.com>
2025-08-26 15:35:48 +0900

    chore: 조건재수정

--- src/mypage/components/StudyApplicants.tsx
+++ src/mypage/components/StudyApplicants.tsx
@@ -63,7 +63,9 @@ export default function StudyApplicants() {
   };
 
   useEffect(() => {
-    fetchApplicantList();
+    if (postIdNum) {
+      fetchApplicantList();
+    }
   }, [postIdNum]);
 
   useEffect(() => {
